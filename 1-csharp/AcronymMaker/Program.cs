575a98225fcb84c67b4155dd96218d0c1c5b007f
David Barnes <david.barnes0402@gmail.com>
2020-11-03 16:23:47 -0500

    complete acronym maker

--- 1-csharp/AcronymMaker/Program.cs
+++ 1-csharp/AcronymMaker/Program.cs
@@ -6,7 +6,33 @@ namespace AcronymMaker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter a string you would like to be turned into an acronym: ");
+			string input = Console.ReadLine();
+
+			var acronym = new System.Text.StringBuilder();
+			while(input != null)
+			{
+				Console.WriteLine(input);
+				//get the character a postition 0
+				acronym.Append(input[0]);
+
+				//get the location of the next space
+				int index = input.IndexOf(" ");
+
+				//check to see if there is anoter space
+				if(index == -1)
+				{
+					// " " was not found in the string and we are at the end
+					// so set the input to null, so we can break out of the loop
+					input = null;
+				}
+				else
+				{
+					input = input.Substring(index + 1);
+				}
+			}
+
+			Console.WriteLine($"The Acronym is: {acronym}");
         }
     }
 }
