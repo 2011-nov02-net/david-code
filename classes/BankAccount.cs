15b23cb5de974292c5b8a7e8749c92e3a84d080c
David Barnes <david.barnes0402@gmail.com>
2020-11-04 09:04:23 -0500

    complete tutorial on classes

--- classes/BankAccount.cs
+++ classes/BankAccount.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace classes
 {
@@ -7,28 +8,75 @@ namespace classes
 		//properties
         public string Number { get; }
 		public string Owner{ get; set; }
-		public decimal balance{ get; }
+		public decimal Balance
+		{ 
+			get
+			{
+				decimal balance = 0;
+				foreach (Transaction item in allTransactions)
+				{
+					balance += item.Amount;
+				}
+
+				return balance;
+			} 
+		}
 
 		private static int accountNumberSeed = 1234567890;
+
+		private List<Transaction> allTransactions = new List<Transaction>();
 
 		//constructor
 		public BankAccount(string name, decimal initialBalance)
 		{
 			this.Owner = name;
-			this.balance = initialBalance;
 			this.Number = accountNumberSeed.ToString();
 			accountNumberSeed++;
+
+			//use the initialBalance as a deposit and add transaction
+			MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
 		}
 
 		//methods
 		public void MakeDeposit(decimal amount, DateTime date, string note)
 		{
-
+			if(amount <= 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive.");
+			}
+			Transaction depotsit = new Transaction(amount, date, note);
+			allTransactions.Add(depotsit);
 		}
 
 		public void MakeWithdrawal(decimal amount, DateTime date, string note)
 		{
+			if(amount <= 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive.");
+			}
+			if(Balance - amount < 0)
+			{
+				throw new InvalidOperationException("Insufficent Funds for this withdrawal.");
+			}
+			Transaction withdrawal = new Transaction(-amount, date, note);
+			allTransactions.Add(withdrawal);
 
+		}
+
+		public string GetAccountHistory()
+		{
+			System.Text.StringBuilder report = new System.Text.StringBuilder();
+
+			decimal balance = 0;
+			report.AppendLine("Date\tAmount\tBalance\tNote");
+
+			foreach(Transaction item in allTransactions)
+			{
+				balance += item.Amount;
+				report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+			}
+
+			return report.ToString();
 		}
     }
 }
--- classes/Program.cs
+++ classes/Program.cs
@@ -8,7 +8,38 @@ namespace classes
         {
             Console.WriteLine("Hello World!");
 			BankAccount acct = new BankAccount("David", 2000);
-			Console.WriteLine($"Account {acct.Number} was created for {acct.Owner} with {acct.balance} inital balance");
-        }
+			Console.WriteLine($"Account {acct.Number} was created for {acct.Owner} with {acct.Balance} inital balance");
+
+			//test deposit and withdrawals
+			acct.MakeWithdrawal(600, DateTime.Now, "Rent");
+			Console.WriteLine(acct.Balance);
+
+			acct.MakeDeposit(500, DateTime.Now, "Refund of expensive purchase");
+			Console.WriteLine(acct.Balance);
+
+			//force an error
+			try
+			{
+				BankAccount test = new BankAccount("Invalid", -55);
+			}
+			catch (ArgumentOutOfRangeException e)
+			{
+				Console.WriteLine("Exception caught while making account");
+				Console.WriteLine(e.ToString());
+			}
+
+			//test for negative balance
+			try
+			{
+				acct.MakeWithdrawal(3000, DateTime.Now, "Attempt to overdraw");
+			}
+			catch (InvalidOperationException e)
+			{
+				Console.WriteLine("Exception caught trying to overdraw");
+				Console.WriteLine(e.ToString());
+			}
+
+			Console.WriteLine(acct.GetAccountHistory());
+		}
     }
 }
